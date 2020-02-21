6f6d8835f1fa16b8028988c6f578e7ba57611046
David Talabard <david.talabard@live.fr>
2020-02-21 11:58:53 +0100

    Query params sample

--- api/others.py
+++ api/others.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
+from typing import Optional
 
 others = APIRouter()
 
 @others.get("/")
-async def read_other_data():
+async def read_other_data(required: str, optional: Optional[str] = None):
     return [{"id": 1, "name": "Test 1"}]
