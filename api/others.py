7ad25f27444cd5ddbfb3828459f41294bce32eba
David Talabard <david.talabard@live.fr>
2020-02-24 12:46:44 +0100

    Endpoint sample with cache

--- api/others.py
+++ api/others.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter
 from typing import Optional
+from cachetools import cached, TTLCache
 
+
+cache = TTLCache(maxsize=100, ttl=300)
 others = APIRouter()
 
 
 @others.get("/")
+@cached(cache)
 def read_other_data(required: str, optional: Optional[str] = None):
     return [{"id": 1, "name": "Test 1"}]
